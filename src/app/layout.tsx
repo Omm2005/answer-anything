4ace4bb5c62ab239425d7891ae9514cb424ae191
Merchnest <shahomm05@gmail.com>
2024-03-15 16:10:33 -0700

    Fixed metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,16 +5,27 @@ import { ThemeProvider } from "./theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const title = "Answer Anything";
+const description = "Ask the supercomputer anything and it will give you the answer. This is inspired by the book &quot;Elon Musk&quot; by Walter Isaacson.";
+const image = "https://omm.needs.rest/answer_it.jpg"
+
 export const metadata: Metadata = {
-  title: "Answer Anything",
-  description: "Ask the supercomputer anything and it will give you the answer. This is inspired by the book &quot;Elon Musk&quot; by Walter Isaacson.",
+  title,
+  description,
+  icons: ["https://omm.needs.rest/favicon-for-website.ico"],
   openGraph: {
-    title: "Answer Anything",
-    description: "Ask the supercomputer anything and it will give you the answer. This is inspired by the book &quot;Elon Musk&quot; by Walter Isaacson.",
-    type: "website",
-    url: 'https://omm.needs.rest/answer_it.jpg',
-    images: 'https://omm.needs.rest/answer_it.jpg',
+    title,
+    description,
+    images: [image],
   },
+  twitter: {
+    card: "summary_large_image",
+    title,
+    description,
+    images: [image],
+    creator: "@maiommhoon",
+  },
+  metadataBase: new URL("https://omm.needs.rest"),
 };
 
 export default function RootLayout({
